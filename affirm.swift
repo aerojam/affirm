ebdc04369d6aaad67e3a7b145cd8282625a8b9c5
Martin Skála <aerojam@://github.com>
2026-05-15 22:33:00 +0200

    Function for adding new affirmations

--- affirm.swift
+++ affirm.swift
@@ -11,17 +11,20 @@ enum Style {
 }
 
 let helpText = """
+
 \(Style.bold)OVERVIEW:\(Style.reset) Affirm – Ranní dávka disciplíny pro Swift vývojáře.
 
 \(Style.bold)USAGE:\(Style.reset) affirm [options]
 
 \(Style.bold)OPTIONS:\(Style.reset)
-  \(Style.yellow)-h, --help\(Style.reset)		Zobrazí tuto nápovědu.
+  \(Style.yellow)-h, --help\(Style.reset)           		   Zobrazí tuto nápovědu.
+  \(Style.yellow)-a, --add Text nové afirmace\(Style.reset)     Přidá novou afirmaci.
 
 \(Style.bold)FILES:\(Style.reset)
   ~/Library/Application Support/Affirm/affirm.txt
-  Soubor s vašimi afirmacemi. Nemůže-li aplikace soubor najít, vytvořte si ho.
-  Každý řádek jedna afirmace.
+  Soubor s vašimi afirmacemi. Nemůže-li aplikace soubor najít, vytvoří ho sama.
+  Každý řádek jedna afirmace. Můžete si doplnit vlastní afirmace.
+
 """
 
 func getFileUrl() throws -> URL {
@@ -60,14 +63,38 @@ func showRandom(from affirmations: [String]) throws {
     print("\(Style.yellow)\(Style.bold)\(randomAffirm)\(Style.reset)")
 }
 
-if CommandLine.arguments.contains("-h") || CommandLine.arguments.contains("--help") {
-    print(helpText)
-    exit(0)
+func addAffirmation(to fileURL: URL, text: String) throws {
+    try ensureFileExist(at: fileURL)
+    let fileHandle = try FileHandle(forWritingTo: fileURL)
+    defer {
+        try? fileHandle.close()
+    }
+    if let data = (text + "\n").data(using: .utf8) {
+        try fileHandle.seekToEnd()
+        try fileHandle.write(contentsOf: data)
+    }
 }
 
 do {
     let fileURL = try getFileUrl()
     try ensureFileExist(at: fileURL)
+    if CommandLine.arguments.contains("-h") || CommandLine.arguments.contains("--help") {
+        print(helpText)
+        exit(0)
+    }
+    if CommandLine.arguments.contains("-a") || CommandLine.arguments.contains("--add") {
+        let args = CommandLine.arguments
+        if args.count >= 3 {
+            let affirmText = CommandLine.arguments[2...].joined(separator: " ")
+            try addAffirmation(to: fileURL, text: affirmText)
+            print("Afirmace byla přidána.")
+            exit(0)
+        } else {
+            print("Nemohl jsem přidat žádnou afirmaci.\n\n")
+            print(helpText)
+            exit(1)
+        }
+    }
     let affirmations = try loadAffirmations(from: fileURL)
     try showRandom(from: affirmations)
 } catch AppError.noAffirmationsFound {
@@ -75,5 +102,3 @@ do {
 } catch {
     print("Něco se nepovedlo: \(error.localizedDescription)")
 }
-
-// Přidat addAffirmation(_: String, to: URL)
